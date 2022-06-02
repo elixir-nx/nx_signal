994301e3055f923f21dc467f929448dc7ff064b7
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-06-02 02:09:57 -0300

    feat: add blackman window

--- lib/nx_signal/windows.ex
+++ lib/nx_signal/windows.ex
@@ -89,8 +89,8 @@ defmodule NxSignal.Windows do
 
       iex> NxSignal.Windows.triangular(n: 3)
       #Nx.Tensor<
-        f64[4]
-        [0.25, 0.75, 0.75, 0.25]
+        f64[3]
+        [0.5, 1.0, 0.5]
       >
   """
   defn triangular(opts \\ []) do
@@ -127,5 +127,88 @@ defmodule NxSignal.Windows do
 
     left = Nx.multiply(idx, 2) |> Nx.subtract(1) |> Nx.divide(n)
     Nx.concatenate([left, Nx.reverse(left)])
+  end
+
+  @doc """
+  Blackman window
+
+  See also: `bartlett/1`
+
+  ## Options
+
+    * `:n` - The window length. Mandatory option.
+    * `:is_periodic` - If `true`, produces a periodic window,
+       otherwise produces a symmetric window. Defaults to `true`
+    * `:type` - the output type for the window. Defaults to `{:f, 32}`
+    * `:name` - the axis name. Defaults to `nil`
+
+  ## Examples
+
+      iex> NxSignal.Windows.blackman(n: 5, is_periodic: false)
+      #Nx.Tensor<
+        f64[5]
+        [-1.4901161193847656e-8, 0.34000001053081275, 0.9999999850988357, 0.34000001053081275, -1.4901161193847656e-8]
+      >
+
+      iex> NxSignal.Windows.blackman(n: 5, is_periodic: true)
+      #Nx.Tensor<
+        f64[5]
+        [-1.4901161193847656e-8, 0.20077014493625245, 0.8492298742686417, 0.8492298742686417, 0.20077014493625245]
+      >
+
+      iex> NxSignal.Windows.blackman(n: 6, is_periodic: true)
+      #Nx.Tensor<
+        f64[6]
+        [-1.4901161193847656e-8, 0.12999999636155424, 0.6300000210936009, 0.9999999850988357, 0.6300000210936009, 0.12999999636155424]
+      >
+  """
+  defn blackman(opts \\ []) do
+    transform(opts, fn opts ->
+      opts = Keyword.validate!(opts, [:n, :name, is_periodic: true, type: {:f, 64}])
+
+      n = opts[:n]
+      name = opts[:name]
+      type = opts[:type]
+      is_periodic = opts[:is_periodic]
+
+      unless n do
+        raise "missing :n option"
+      end
+
+      blackman(n, name, type, is_periodic)
+    end)
+  end
+
+  defp blackman(l, name, type, is_periodic) do
+    import Nx.Defn.Kernel, only: [/: 2, *: 2, +: 2, -: 2]
+    import Kernel, except: [/: 2, *: 2, +: 2, -: 2]
+
+    l =
+      if is_periodic do
+        l + 1
+      else
+        l
+      end
+
+    m = ceil(l / 2)
+
+    n = Nx.iota({m}, names: [name], type: type)
+
+    left =
+      0.42 - 0.5 * Nx.cos(2 * :math.pi() * n / (l - 1)) +
+        0.08 * Nx.cos(4 * :math.pi() * n / (l - 1))
+
+    window =
+      if rem(l, 2) == 0 do
+        Nx.concatenate([left, Nx.reverse(left)])
+      else
+        Nx.concatenate([left, left |> Nx.reverse() |> Nx.slice([1], [Nx.size(left) - 1])])
+      end
+
+    if is_periodic do
+      Nx.slice(window, [0], [Nx.size(window) - 1])
+    else
+      window
+    end
   end
 end
