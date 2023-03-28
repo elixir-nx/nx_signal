c7ca95d8a87c99912f0fd8cfcc1550cfbe636531
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-03-28 17:26:01 -0300

    fix: mix.exs version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NxSignal.MixProject do
     [
       app: :nx_signal,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
