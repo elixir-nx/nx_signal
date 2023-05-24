2c2d0c20a15fb6b50d5db7404959159769094fc8
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2023-05-24 18:27:46 -0300

    feat: square

--- lib/nx_signal/waveforms.ex
+++ lib/nx_signal/waveforms.ex
@@ -5,7 +5,6 @@ defmodule NxSignal.Waveforms do
   import Nx.Defn
   import Nx.Constants, only: [pi: 0]
 
-  # square
   # gauss_pulse
   # chirp
   # sweep_poly
@@ -14,8 +13,8 @@ defmodule NxSignal.Waveforms do
   Periodic sawtooth or triangular waveform.
 
   The wave as a period of $2\\pi$, rising from -1 to 1
-  on the interval $[0, 2\\piwidth]$ and dropping from
-  1 to -1 on the interval $[2\\piwidth, 2\\pi]$.
+  in the interval $[0, 2\\piwidth]$ and dropping from
+  1 to -1 in the interval $[2\\piwidth, 2\\pi]$.
 
   ## Options
 
@@ -55,5 +54,28 @@ defmodule NxSignal.Waveforms do
           (pi() * (width + 1) - tmod) / (pi() * (1 - width))
         )
     end
+  end
+
+  @doc """
+  A periodic square wave with period $2\\pi$.
+
+  Evaluates to 1 in the interval $[0, 2\\pi\\text{duty}]$
+  and -1 in the interval $[2\\pi\\text{duty}, 2\\pi]$.
+
+  ## Options
+
+    * `:duty` - a number or tensor representing the duty cycle.
+    If a tensor is given, the waveform changes over time, and it
+    must have the same length as the `t` input. Defaults to `0.5`.
+  """
+  deftransform square(t, opts \\ []) do
+    opts = Keyword.validate!(opts, duty: 0.5)
+    square_n(t, opts[:duty])
+  end
+
+  defnp square_n(t, duty) do
+    tmod = Nx.remainder(t, 2 * pi())
+
+    Nx.select(tmod < duty * 2 * pi(), 1, -1)
   end
 end
