228085381973f5e4e792983255235a0a6a10963c
Santiago Imelio <santiago.imelio@gmail.com>
2024-05-11 16:51:14 -0300

    refactor median filter, move sinc to waveforms

--- lib/nx_signal/filters.ex
+++ lib/nx_signal/filters.ex
@@ -4,78 +4,92 @@ defmodule NxSignal.Filters do
   """
   import Nx.Defn
 
-  @pi :math.pi()
-
   @doc ~S"""
-  Calculates the normalized sinc function $sinc(t) = \frac{sin(\pi t)}{\pi t}$
+  Performs a median filter on a rank 1 or rank 2 tensor.
 
-  ## Examples
+  ## Options
 
-      iex> NxSignal.Filters.sinc(Nx.tensor([0, 0.25, 1]))
-      #Nx.Tensor<
-        f32[3]
-        [1.0, 0.9003162980079651, -2.7827534054836178e-8]
-      >
+    * `:kernel_shape` - the shape of the sliding window.
+    It must be compatible with the shape of the tensor.
   """
   @doc type: :filters
-  defn sinc(t) do
-    t = t * @pi
-    zero_idx = Nx.equal(t, 0)
-
-    # Define sinc(0) = 1
-    Nx.select(zero_idx, 1, Nx.sin(t) / t)
+  def median(t = %Nx.Tensor{shape: {length}}, opts) do
+    validate_median_opts!(t, opts)
+    {kernel_length} = opts[:kernel_shape]
+    median(Nx.reshape(t, {1, length}), kernel_shape: {1, kernel_length})
   end
 
-  defn median_filter(t, opts \\ [kernel_size: 3]) do
+  def median(t = %Nx.Tensor{shape: {_h, _w}}, opts) do
+    validate_median_opts!(t, opts)
+    median_n(t, opts)
+  end
+
+  def median(_t, _opts), do: raise(ArgumentError, message: "tensor must be of rank 1 or 2")
+
+  defnp median_n(t, opts) do
+    kernel_shape = opts[:kernel_shape]
+
     {height, width} = Nx.shape(t)
-    kernel_tensor = Nx.broadcast(0.0, {opts[:kernel_size]})
+
+    kernel_tensor = Nx.broadcast(0.0, kernel_shape)
     output = Nx.broadcast(0.0, Nx.shape(t))
 
-    {_, _, _, _, _, result} =
-      while {i = 0, t, kernel_tensor, height, width, output}, i < height do
+    {result, _} =
+      while {output, {i = 0, t, kernel_tensor, height, width}}, i < height do
         row = Nx.broadcast(0.0, {elem(Nx.shape(t), 1)})
 
-        {_, _, _, _, _, ith_row} =
-          while {j = 0, t, i, kernel_tensor, width, row}, j < width do
-            ij_median =
-              ij_window_median(t, i, j, kernel_tensor)
+        {ith_row, _} =
+          while {row, {j = 0, t, i, kernel_tensor, width}}, j < width do
+            median =
+              window_median(t, i, j, kernel_tensor)
               |> Nx.broadcast({1})
 
-            {j + 1, t, i, kernel_tensor, width, Nx.put_slice(row, [j], ij_median)}
+            {Nx.put_slice(row, [j], median), {j + 1, t, i, kernel_tensor, width}}
           end
 
-        {i + 1, t, kernel_tensor, height, width,
-         Nx.put_slice(output, [i, 0], Nx.stack(ith_row, axis: 0))}
+        {Nx.put_slice(output, [i, 0], Nx.stack(ith_row, axis: 0)),
+         {i + 1, t, kernel_tensor, height, width}}
       end
 
-    result
+    Nx.squeeze(result)
   end
 
-  defnp ij_window_median(t, i, j, kernel_tensor) do
-    {kernel_size} = Nx.shape(kernel_tensor)
-    window_pad_size = Nx.round((kernel_size - 1) / 2)
+  defnp window_median(t, i, j, kernel_tensor) do
+    kernel_shape = Nx.shape(kernel_tensor)
+    kernel_dims = [elem(kernel_shape, 0), elem(kernel_shape, 1)]
+
+    padding_y = Nx.round((elem(kernel_shape, 0) - 1) / 2)
+    padding_x = Nx.round((elem(kernel_shape, 1) - 1) / 2)
 
     y_axis_start_idx =
-      if i - window_pad_size <= 0 do
+      if i - padding_y <= 0 do
         0
       else
-        i - window_pad_size
+        i - padding_y
       end
       |> Nx.as_type({:u, 32})
 
     x_axis_start_idx =
-      if j - window_pad_size <= 0 do
+      if j - padding_x <= 0 do
         0
       else
-        j - window_pad_size
+        j - padding_x
       end
       |> Nx.as_type({:u, 32})
 
     Nx.slice(
       t,
       [y_axis_start_idx, x_axis_start_idx],
-      [kernel_size, kernel_size]
+      kernel_dims
     )
     |> Nx.median()
+  end
+
+  defn validate_median_opts!(t, opts) do
+    keyword!(opts, [:kernel_shape])
+
+    if Nx.rank(t) != Nx.rank(opts[:kernel_shape]) do
+      raise ArgumentError, message: "kernel shape must be of the same rank as the tensor"
+    end
   end
 end
