d0b7df4e6f69e442787aa13ca1ec1d530f245724
Santiago Imelio <82551777+santiago-imelio@users.noreply.github.com>
2024-05-17 23:46:25 -0300

    Add Median Filter 2D (#20)
    * bump nx to 0.7
    
    * implement median filter 2D
    
    * refactor median filter, move sinc to waveforms
    
    * use deftransform, minor tweaks
    
    * use vectorization, add tests
    
    * run mix format

--- lib/nx_signal/filters.ex
+++ lib/nx_signal/filters.ex
@@ -4,25 +4,52 @@ defmodule NxSignal.Filters do
   """
   import Nx.Defn
 
-  @pi :math.pi()
-
   @doc ~S"""
-  Calculates the normalized sinc function $sinc(t) = \frac{sin(\pi t)}{\pi t}$
+  Performs a median filter on a rank 1 or rank 2 tensor.
 
-  ## Examples
+  ## Options
 
-      iex> NxSignal.Filters.sinc(Nx.tensor([0, 0.25, 1]))
-      #Nx.Tensor<
-        f32[3]
-        [1.0, 0.9003162980079651, -2.7827534054836178e-8]
-      >
+    * `:kernel_shape` - the shape of the sliding window.
+    It must be compatible with the shape of the tensor.
   """
   @doc type: :filters
-  defn sinc(t) do
-    t = t * @pi
-    zero_idx = Nx.equal(t, 0)
+  deftransform median(t = %Nx.Tensor{shape: {length}}, opts) do
+    validate_median_opts!(t, opts)
+    {kernel_length} = opts[:kernel_shape]
 
-    # Define sinc(0) = 1
-    Nx.select(zero_idx, 1, Nx.sin(t) / t)
+    median(Nx.reshape(t, {1, length}), kernel_shape: {1, kernel_length})
+    |> Nx.squeeze()
+  end
+
+  deftransform median(t = %Nx.Tensor{shape: {_h, _w}}, opts) do
+    validate_median_opts!(t, opts)
+    median_n(t, opts)
+  end
+
+  deftransform median(_t, _opts),
+    do: raise(ArgumentError, message: "tensor must be of rank 1 or 2")
+
+  defn median_n(t, opts) do
+    {k0, k1} = opts[:kernel_shape]
+
+    idx =
+      Nx.stack([Nx.iota(t.shape, axis: 0), Nx.iota(t.shape, axis: 1)], axis: -1)
+      |> Nx.reshape({:auto, 2})
+      |> Nx.vectorize(:elements)
+
+    t
+    |> Nx.slice([idx[0], idx[1]], [k0, k1])
+    |> Nx.median()
+    |> Nx.devectorize(keep_names: false)
+    |> Nx.reshape(t.shape)
+    |> Nx.as_type({:f, 32})
+  end
+
+  deftransformp validate_median_opts!(t, opts) do
+    Keyword.validate!(opts, [:kernel_shape])
+
+    if Nx.rank(t) != Nx.rank(opts[:kernel_shape]) do
+      raise ArgumentError, message: "kernel shape must be of the same rank as the tensor"
+    end
   end
 end
--- lib/nx_signal/waveforms.ex
+++ lib/nx_signal/waveforms.ex
@@ -435,4 +435,24 @@ defmodule NxSignal.Waveforms do
         Nx.reshape(index, {n})
     end
   end
+
+  @doc ~S"""
+  Calculates the normalized sinc function $sinc(t) = \frac{sin(\pi t)}{\pi t}$
+
+  ## Examples
+
+      iex> NxSignal.Waveforms.sinc(Nx.tensor([0, 0.25, 1]))
+      #Nx.Tensor<
+        f32[3]
+        [1.0, 0.9003162980079651, -2.7827534054836178e-8]
+      >
+  """
+  @doc type: :waveforms
+  defn sinc(t) do
+    t = t * pi()
+    zero_idx = Nx.equal(t, 0)
+
+    # Define sinc(0) = 1
+    Nx.select(zero_idx, 1, Nx.sin(t) / t)
+  end
 end
