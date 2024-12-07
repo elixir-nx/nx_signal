e30062b2be0315e2939ebf931b5dda723e3f5966
Theron Boerner <hunterboerner@gmail.com>
2024-12-07 12:47:59 -0500

    stub implementation

---
+++ lib/nx_signal/convolution.ex
@@ -0,0 +1,10 @@
+defmodule NxSignal.Convolution do
+  @moduledoc """
+  Convolution functions
+  """
+
+  import Nx.Defn
+
+  deftransform convolve(in1, in2, mode \\ "full", method \\ "auto") do
+  end
+end
