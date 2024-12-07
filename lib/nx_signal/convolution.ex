b8599f564008257a374d209d73bd85d8d704b697
Theron Boerner <hunterboerner@gmail.com>
2024-12-07 13:08:13 -0500

    Attempt at implementing convolve

--- lib/nx_signal/convolution.ex
+++ lib/nx_signal/convolution.ex
@@ -5,6 +5,44 @@ defmodule NxSignal.Convolution do
 
   import Nx.Defn
 
-  deftransform convolve(in1, in2, mode \\ "full", method \\ "auto") do
+  deftransform convolve(kernel, in2, mode \\ "full", method \\ "auto") do
+    axes =
+      Nx.axes(kernel)
+
+    kernel =
+      if Nx.Type.complex?(Nx.type(kernel)) do
+        Nx.conjugate(Nx.reverse(kernel, axes: axes))
+      else
+        Nx.reverse(kernel, axes: axes)
+      end
+
+    kernel =
+      case Nx.shape(kernel) do
+        {m} ->
+          Nx.reshape(kernel, {1, 1, m})
+
+        {m, n} ->
+          nil
+          Nx.reshape(kernel, {1, m, n})
+
+        _ ->
+          kernel
+      end
+
+    in2 =
+      case Nx.shape(in2) do
+        {m} ->
+          Nx.reshape(in2, {1, 1, m})
+
+        {m, n} ->
+          nil
+          Nx.reshape(in2, {1, m, n})
+
+        _ ->
+          in2
+      end
+
+    Nx.conv(kernel, in2)
+    |> Nx.squeeze()
   end
 end
