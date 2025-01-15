d120463acd46c51305c72f475bde42e4d7db74f3
Theron Boerner <hunterboerner@gmail.com>
2025-01-15 15:32:34 -0500

    work on lambert_w

--- lib/nx_signal/internal.ex
+++ lib/nx_signal/internal.ex
@@ -6,6 +6,8 @@ defmodule NxSignal.Internal do
   @expn1 0.36787944117144232159553
 
   defn lambert_w(z, k, tol \\ 1.0e-8) do
+    # z = Nx.as_type(z, z |> Nx.type() |> Nx.Type.to_complex())
+    z = Nx.as_type(z, {:c, 128})
     rz = Nx.real(z)
 
     cond do
@@ -86,7 +88,8 @@ defmodule NxSignal.Internal do
         {w, _} =
           while {w, {z, tol, i = 0}}, i < 100 do
             ew = Nx.exp(w)
-            wewz = w * ew
+            wew = w * ew
+            wewz = wew - z
             wn = w - wewz / (wew + ew - (w + 2.0) * wewz / (2.0 * w + 2.0))
 
             if Nx.abs(wn - w) <= tol * Nx.abs(wn) do
@@ -101,14 +104,32 @@ defmodule NxSignal.Internal do
   end
 
   defnp lambertw_branchpt(z) do
-    # TODO
+    m_e =
+      z
+      |> Nx.type()
+      |> Nx.Type.to_real()
+      |> Nx.Constants.e()
+
+    p = Nx.sqrt(2.0 * (m_e * z + 1.0))
+
+    cevalpoly_2(p, -1.0 / 3.0, 1.0, -1.0)
   end
 
   defnp lambertw_pade0(z) do
-    # TODO
+    z * cevalpoly_2(z, 12.85106382978723404255, 12.34042553191489361902, 1.0) /
+      cevalpoly_2(z, 32.53191489361702127660, 14.34042553191489361702, 1.0)
   end
 
   defnp lambertw_asy(z, k) do
-    # TODO
+    w = Nx.log(z) + 2.0 * Nx.Constants.pi() * k * Nx.Constants.i()
+    w - Nx.log(w)
+  end
+
+  defnp cevalpoly_2(z, c0, c1, c2) do
+    s = Nx.abs(z)
+    r = 2 * Nx.real(z)
+    b = -s * c0 + c2
+    a = r * c0 + c1
+    z * a + b
   end
 end
