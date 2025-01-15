b210434484d3404b30ddf4ab1a30c8776c003d6b
Theron Boerner <hunterboerner@gmail.com>
2025-01-15 14:29:23 -0500

    lambert w test

---
+++ test/nx_signal/internal_test.exs
@@ -0,0 +1,30 @@
+defmodule NxSignal.InternalTest do
+  use NxSignal.Case, async: true, validate_doc_metadata: false
+
+  describe "lambert_w/3" do
+    for {a, b, y} <- [
+          {0, 0, 0},
+          {Complex.new(0, 0), 0, 0},
+          {:infinity, 0, Complex.new(:infinity, 0)},
+          {0, -1, Complex.new(:neg_infinity, 0)},
+          {0, 1, Complex.new(:neg_infinity, 0)},
+          {0, 3, Complex.new(:neg_infinity, 0)},
+          {Nx.to_number(Nx.Constants.e()), 0, 1},
+          {1, 0, 0.567143290409783873},
+          {-Nx.to_number(Nx.Constants.pi()) / 2, 0,
+           Complex.new(0, Nx.to_number(Nx.Constants.pi()) / 2)},
+          {-Nx.to_number(Nx.log(2)) / 2, 0, -Nx.to_number(Nx.log(2))},
+          {0.25, 0, 0.203888354702240164},
+          {-0.25, 0, -0.357402956181388903},
+          {-1.0 / 10000, 0, -0.000100010001500266719},
+          {-0.25, -1, -2.15329236411034965},
+          {0.25, -1, Complex.new(-3.00899800997004620, -4.07652978899159763)},
+          {-0.25, -1, -2.15329236411034965},
+          {0.25, 1, Complex.new(-3.00899800997004620, 4.07652978899159763)},
+          {-0.25, 1, Complex.new(-3.48973228422959210, 7.41405453009603664)}
+        ] do
+      x = lambert_w(a, b)
+      assert_all_close(x, y, atol: 1.0e-13, rtol: 1.0e-10)
+    end
+  end
+end
